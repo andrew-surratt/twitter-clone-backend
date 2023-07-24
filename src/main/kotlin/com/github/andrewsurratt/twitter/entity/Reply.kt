0364b6d681630e3c5eea7aa1ec6bb9bbb4ebbe8f
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2023-07-23 23:43:26 -0400

    Create replies to tweets: DB migration, repo, entity, JPA relationship, and controller.

--- src/main/kotlin/com/github/andrewsurratt/twitter/entity/Reply.kt
+++ src/main/kotlin/com/github/andrewsurratt/twitter/entity/Reply.kt
@@ -6,27 +6,31 @@ import jakarta.persistence.*
 import java.util.*
 
 @Entity
-@Table(name = "tweet", schema = "twitter")
-class Tweet() {
+@Table(name = "replies", schema = "twitter")
+class Reply() {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "tweet_id")
-    lateinit var tweetId: UUID;
+    @Column(name = "reply_id")
+    lateinit var replyId: UUID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tweet_id")
+    @JsonBackReference
+    lateinit var tweet: Tweet;
+
     @Column(name = "user_id")
     lateinit var userId: UUID;
-    var tweet: String = "";
+    var reply: String = "";
     var created: Date = Date();
 
-    @OneToMany(mappedBy = "tweet")
-    @JsonManagedReference
-    var replies: List<Reply> = emptyList();
-
     constructor(
+        tweet: Tweet,
         userId: UUID,
-        tweet: String
+        reply: String
     ) : this() {
-        this.userId = userId;
         this.tweet = tweet;
+        this.userId = userId;
+        this.reply = reply;
         this.created = Date();
     }
 }
